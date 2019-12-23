311c0763b0b712d3a55043ab12cca15c661c4c02
George Blackmore <gtblackmore@gmail.com>
2019-12-23 05:05:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,10 +33,7 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == true
-      return name
-    else
-      self.create_by_name(name)
+    if self.find_by_name(name) || self.create_by_name(name)
     end
   end
 end
