0a735e79fdc5635d705fe8ed10365d996f55dfd3
George Blackmore <gtblackmore@gmail.com>
2019-12-23 05:23:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,5 +43,7 @@ class Song
   def self.new_from_filename(filename)
     file = filename.split(" - ")
     artist_name = file[0]
-    song_name = file[1].delete".mp3"
+    song_name = file[1].delete ".mp3"
+    
+    
 end
